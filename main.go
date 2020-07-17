b42a923896365e6e455de0d4cd7d74e3677ad793
takagotch <dyaccb@gmail.com>
2020-07-18 02:27:50 +0900

    1st

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "log"
+  "net/http"
+)
+
+func main() {
+  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FileServer(http.Dir("/etc/gohttpserver/doc")))))
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+/*
+
+func main() {
+  http.Handle("/static/",
+    http.StripPrefix("/static/",
+      http.FileServer(
+        http.Dir("/etc/gohttpserver/doc"),
+      ),
+    ),
+  )
+
+  log.Fatal(
+    http.ListenAndServe(":8080", nil),
+  )
+}
+
+*/
+
